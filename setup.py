630047ed6a3447376d0e7bb4845473b919b97d0b
Eric Jara <eric.jose.jara.palacios961109@gmail.com>
2025-08-09 18:56:41 -0500

    primer lanzamiento para nuestro paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PackageZeki",                                 # Nombre del paquete
+    version="0.1.0",                                    # Versión inicial
+    packages=find_packages(),                           # Paquetes a incluir
+    description="Un paquete pip simple de saludo",      # Breve descripción
+    author="TheZeki96",                                 # Tu nombre
+    author_email="eric.jara.p@uni.pe",                  # Tu correo electrónico
+    url="https://github.com/TheZeki96/PackageZeki",     # URL del proyecto
+)
